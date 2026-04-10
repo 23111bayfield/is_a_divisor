00285b5da4c48bbd2a37e7a1a72cbdc4010da956
23111 <23111@bayfield-high.school.nz>
2026-04-10 12:19:56 +1200

    forgot comments

--- divisor.py
+++ divisor.py
@@ -1,12 +1,15 @@
-integer_input = int(input(""))
+# Write a program that asks for an initial integer. The program then repeatedly prompts for integers. If the integer i is a divisor of the initial integer, print 'i is a divisor'.
+# If the integer i is not a divisor of the initial integer, print 'i is not a divisor'. The program should continue until it is given the value 0 as input.
 
-while True:
+integer_input = int(input("")) # store user input in variable
+
+while True: # initiate while loop 
     following_num = int(input(""))
     
-    if following_num == 0:
+    if following_num == 0: # if 0, end program
         break
         
-    if integer_input % following_num == 0:
+    if integer_input % following_num == 0: # if integer_input divided by following_num = 0, print "is a divisor". otherwise, print "is not a divisor".
         print(f"{following_num} is a divisor")
     else:
         print(f"{following_num} is not a divisor")
